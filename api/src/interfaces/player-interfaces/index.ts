b7ce216a25fc6e48b527fa9aad2fb613772b3b2e
Seba <sebastian.pitra10@gmail.com>
2023-02-15 14:17:02 -0300

    fix(interfaces/player): add extend interfaces

--- api/src/interfaces/player-interfaces/index.ts
+++ api/src/interfaces/player-interfaces/index.ts
@@ -1,14 +1,16 @@
-export interface IPlayerResponse {
+export interface IMessage {
   msg: string;
+}
+
+export interface IRoomId {
+  roomId: string;
+}
+
+export interface IPlayerResponse extends IMessage {
   playerId: string;
 }
 
-export interface IError {
-  msg: string;
-}
-
-export interface IParamsUpdatePlayer {
-  roomId: string;
+export interface IParamsUpdatePlayer extends IRoomId {
   isPlayerOne: boolean;
   online: boolean;
   start: boolean;
