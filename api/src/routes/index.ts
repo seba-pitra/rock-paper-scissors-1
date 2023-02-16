7a42e69debc5747dcbe6b2fd0da553ac646c2da0
Seba <sebastian.pitra10@gmail.com>
2023-02-16 11:15:37 -0300

    fix: game files deleted

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import playerRouter from "./player-router";
 import roomsRouter from "./rooms-router";
-import gameRouter from "./game-router";
 
 const router: Router = Router();
 
 router.use("/player", playerRouter);
 router.use("/rooms", roomsRouter);
-router.use("/game", gameRouter);
 
 export default router;
