0caf9e77bba60ac201900035b718a2ddbea69a8f
Seba <sebastian.pitra10@gmail.com>
2023-02-28 22:53:45 -0300

    fix: player router formatte

--- api/src/routes/player-router.ts
+++ api/src/routes/player-router.ts
@@ -4,10 +4,10 @@ import PlayerController from "../controllers/player-controller";
 const playerRouter = Router();
 const playerController = new PlayerController();
 
-playerRouter.post("/signup", playerController.signUp);
+playerRouter.post("/signup",             playerController.signUp);
 playerRouter.post("/new-player/:roomId", playerController.addPlayerTwo);
-playerRouter.put("/status/:roomId", playerController.updatePlayerStatus);
-playerRouter.put("/play/:roomId", playerController.updatePlayerChoose);
-playerRouter.put("/history/:roomId", playerController.updatePlayerHistory);
+playerRouter.put("/status/:roomId",      playerController.updatePlayerStatus);
+playerRouter.put("/play/:roomId",        playerController.updatePlayerChoose);
+playerRouter.put("/history/:roomId",     playerController.updatePlayerHistory);
 
 export default playerRouter;
