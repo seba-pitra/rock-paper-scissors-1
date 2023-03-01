e3bbd5f1e05ce93cfb54dee8373d8e839965e99c
Seba <sebastian.pitra10@gmail.com>
2023-02-28 22:45:28 -0300

    fix(playerRouter): method names changed

--- api/src/routes/player-router.ts
+++ api/src/routes/player-router.ts
@@ -5,7 +5,9 @@ const playerRouter = Router();
 const playerController = new PlayerController();
 
 playerRouter.post("/signup", playerController.signUp);
-playerRouter.post("/new-player", playerController.addPlayerTwo);
+playerRouter.post("/new-player/:roomId", playerController.addPlayerTwo);
 playerRouter.put("/status/:roomId", playerController.updatePlayerStatus);
+playerRouter.put("/play/:roomId", playerController.updatePlayerChoose);
+playerRouter.put("/history/:roomId", playerController.updatePlayerHistory);
 
 export default playerRouter;
